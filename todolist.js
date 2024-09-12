41af8846b512eb8f4407ce65a600c895f4752b65
Kings <ebucekings62@gmail.com>
2024-09-12 06:07:39 -1200

    changes

--- todolist.js
+++ todolist.js
@@ -1,28 +1,11 @@
-const inputDisplay = [{name:'cook dinner',duedate: '05-20-2024'}, {name: 'wash cloths', duedate: '05-20-2024'}];
+let inputDisplay = [];
 
-// function renderTodo() {
-//     let todolist = '';
-//     for (let i = 0; i < inputDisplay.length; i++) {
-//        const todoObject = inputDisplay[i];
-//        const name = todoObject.name;
-//        const duedate = todoObject.duedate;
-
-    //    const html = `
-    //    <div>${name}</div>
-    //    <div>${duedate}</div>
-    //    <div>
-    //    <button onclick="inputDisplay.splice(${i}, 1);
-    //    renderTodo()
-    //    ">Delete</button>
-    //    </div>
-    //    `
-//        todolist += html;
-// }
-// document.querySelector('.todo-grid')
-//         .innerHTML = todolist;
-
-//  }
-
+// Function to save to localStorage
+function addToStorage() {
+    localStorage.setItem('inputDisplay', JSON.stringify(inputDisplay));
+}
+// document.querySelector(".js-error").innerHTML = '';
+// Function to render to-dos
 function renderTodo() {
     let todolist = '';
     inputDisplay.forEach((input, index) => {
@@ -32,24 +15,49 @@ function renderTodo() {
             <div>
                 <button class="delete-button" onclick="deleteTodo(${index})">Delete</button>
             </div>
+            <div class="js-error"></div>
         `;
         todolist += html;
     });
     document.querySelector('.todo-grid').innerHTML = todolist;
 }
 
+// Function to delete a to-do
 function deleteTodo(index) {
     inputDisplay.splice(index, 1);
     renderTodo();
+    addToStorage();
 }
 
-
+// Function to handle adding a new to-do
 function clickTodo() {
     const inputElem = document.querySelector(".js-input");
     const name = inputElem.value;
     const inputdate = document.querySelector(".js-duedate");
-    const duedate = inputdate.value
-    inputDisplay.push({name: name, duedate: duedate});
-    console.log(inputDisplay);
-    renderTodo();
-};
+    const duedate = inputdate.value;
+
+    if (name && duedate) { // Check if values are not empty
+        inputDisplay.push({name: name, duedate: duedate});
+        inputElem.value = ''; 
+        inputdate.value = ''; 
+        renderTodo();
+        addToStorage();
+    } else {
+        // alert('Please fill in both fields.');
+        document.querySelector(".js-error").innerHTML = "Please fill in both fields.";
+    }
+}
+
+// Load data from localStorage on page load
+function loadFromStorage() {
+    const storedDataString = localStorage.getItem('inputDisplay');
+    if (storedDataString) {
+        inputDisplay = JSON.parse(storedDataString);
+        renderTodo();
+    } else {
+        console.log('No data found in localStorage.');
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', loadFromStorage);
